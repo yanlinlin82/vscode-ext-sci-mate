d6b696db3e862d5344bfbf5923cda8153e1beb9f
Linlin Yan <linlin.yan@bioinfo.app>
2024-03-18 10:56:10 +0800

    Add DOI link provider.

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,26 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+class DoiLinkProvider {
+	provideDocumentLinks(document, token) {
+		const text = document.getText();
+		const doiPattern = /\bdoi:10\.\d{4,9}\/[-._;()/:A-Z0-9]+/gi;
+		let links = [];
+
+		let match;
+		while ((match = doiPattern.exec(text)) !== null) {
+			const startPos = document.positionAt(match.index);
+			const endPos = document.positionAt(match.index + match[0].length);
+			const range = new vscode.Range(startPos, endPos);
+			const uri = vscode.Uri.parse(`https://doi.org/${match[0].substring(4)}`);
+			const link = new vscode.DocumentLink(range, uri);
+			links.push(link);
+		}
+
+		return links;
+	}
+}
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -18,8 +38,11 @@ export function activate(context: vscode.ExtensionContext) {
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from Science Mate!');
 	});
-
 	context.subscriptions.push(disposable);
+
+	let provider = new DoiLinkProvider();
+	let disposable2 = vscode.languages.registerDocumentLinkProvider({ scheme: 'file', language: 'markdown' }, provider);
+	context.subscriptions.push(disposable2);
 }
 
 // This method is called when your extension is deactivated
